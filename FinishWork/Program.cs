6aa9b4b35c9c398a24674c986618b0b4feb03873
Aleksandr <ale240489@yandex.ru>
2022-11-26 17:36:02 +0300

    Finish work

---
+++ FinishWork/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] FinishArray (string[] arrayString)
+{
+    int index = 0;
+    int length = 0;
+    for (int i = 0; i < arrayString.Length; i++)
+        {
+            if(arrayString[i].Length<=3)
+                length++;
+        }
+    string[] finishArrayString = new string[length];
+    for (int i = 0; i < arrayString.Length; i++)
+        {
+            if(arrayString[i].Length<=3)
+            {
+                finishArrayString[index]=arrayString[i];
+                index++;
+            }
+        }
+    return finishArrayString;
+}
+
+void PrintArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+string[] arrayString = {"Гоша", "Лена","Нож","Кот","Сом","Виктор","Хом"};
+string[] finishArray = FinishArray(arrayString);
+PrintArray(finishArray);
+
+
+
